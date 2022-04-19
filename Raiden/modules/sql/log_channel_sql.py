3677d791f1a886408a1cc75a09ddb78759c47964
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-20 03:08:23 +0530

    Update log_channel_sql.py

--- Raiden/modules/sql/log_channel_sql.py
+++ Raiden/modules/sql/log_channel_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from KomiXRyu.modules.sql import BASE, SESSION
+from Raiden.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
