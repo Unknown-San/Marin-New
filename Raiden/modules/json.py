b998be7dea7dce54e8f65c7ec36bcd3337c645b2
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-20 01:41:53 +0530

    Raiden

--- Raiden/modules/json.py
+++ Raiden/modules/json.py
@@ -6,8 +6,8 @@ from telethon import types
 from telethon.tl import functions, types
 
 
-from KomiXRyu.events import register
-from KomiXRyu import telethn
+from Raiden.events import register
+from Raiden import telethn
 
 
 async def is_register_admin(chat, user):
