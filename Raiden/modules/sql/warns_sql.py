9a4d55dca6bb6d6a54227a6c4d57765a40d4d154
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-20 03:16:40 +0530

    Update warns_sql.py

--- Raiden/modules/sql/warns_sql.py
+++ Raiden/modules/sql/warns_sql.py
@@ -4,7 +4,7 @@ import threading
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql.sqltypes import BigInteger
-from KomiXRyu.modules.sql import SESSION, BASE
+from Raiden.modules.sql import SESSION, BASE
 
 
 class Warns(BASE):
