990ddad44241be73135e0eaa8948518a1f242db9
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-20 03:15:07 +0530

    Update reporting_sql.py

--- Raiden/modules/sql/reporting_sql.py
+++ Raiden/modules/sql/reporting_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.sql.sqltypes import BigInteger
-from KomiXRyu.modules.sql import SESSION, BASE
+from Raiden.modules.sql import SESSION, BASE
 
 
 class ReportingUserSettings(BASE):
