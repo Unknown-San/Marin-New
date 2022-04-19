0e27bdb912fff62b3b7182ba23cf5cf748472717
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-20 02:37:35 +0530

    Raiden

--- Raiden/modules/tagger.py
+++ Raiden/modules/tagger.py
@@ -3,8 +3,8 @@ import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from KomiXRyu import client as telethn
-from KomiXRyu.events import register as nobara
+from Raiden import client as telethn
+from Raiden.events import register as nobara
 
 from telegram.utils.helpers import escape_markdown, mention_html, mention_markdown
 
