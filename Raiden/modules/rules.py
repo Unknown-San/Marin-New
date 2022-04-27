47121c4a57cd97c1927a5c3b907017251ac46e41
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-28 01:24:40 +0530

    Stats

--- Raiden/modules/rules.py
+++ Raiden/modules/rules.py
@@ -95,7 +95,7 @@ def clear_rules(update, context):
 
 
 def __stats__():
-    return "× {} chats have rules set.".format(sql.num_chats())
+    return "» {} Chats have rules set.".format(sql.num_chats())
 
 
 def __import_data__(chat_id, data):
