5406a2c16cbfb4ffa4f96f92867f1aeb45769c95
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-28 03:18:07 +0530

    Trying to be kuul

--- Raiden/modules/misc.py
+++ Raiden/modules/misc.py
@@ -116,9 +116,9 @@ def info(update, context):
     )
 
     text = (
-        "╒═══ About {}".format(user.id, html.escape(user.first_name))
-        "\n\n »Id: <code>{}</code>"
-        "\n »First Name: {}".format(user.id, html.escape(user.first_name))
+        f"╒═══「<b> About {html.escape(user.first_name)} 」\n"
+        f"» ID: <code>{}</code>\n"
+        f"» First Name: {html.escape(user.first_name)}"
     )
 
     if user.last_name:
