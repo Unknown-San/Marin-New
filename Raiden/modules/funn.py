3d898c52e40ad4ebc8c7cfccc72736ec61e0118e
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-05-13 12:10:32 +0530

    Update funn.py

--- Raiden/modules/funn.py
+++ Raiden/modules/funn.py
@@ -1,7 +1,7 @@
 import html
 import random
 import time
-
+from Raiden.modules.helper_funcs.chat_status import dev_plus
 import Raiden.modules.fun_strings as fun_strings
 from Raiden import dispatcher
 from Raiden.modules.disable import DisableAbleCommandHandler
