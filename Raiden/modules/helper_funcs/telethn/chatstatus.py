c402e3c5bff3022f3f19756be1326f940a4abd22
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-05-23 19:32:35 +0530

    Update chatstatus.py

--- Raiden/modules/helper_funcs/telethn/chatstatus.py
+++ Raiden/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
 from Raiden.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from Raiden import DRAGONS
+from Raiden import SUPPORT_USERS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
@@ -23,7 +23,7 @@ async def user_is_admin(user_id: int, message):
 
     async for user in telethn.iter_participants(
             message.chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in DRAGONS:
+        if user_id == user.id or user_id in SUPPORT_USERS:
             status = True
             break
     return status
@@ -33,7 +33,7 @@ async def is_user_admin(user_id: int, chat_id):
     status = False
     async for user in telethn.iter_participants(
             chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in DRAGONS:
+        if user_id == user.id or user_id in SUPPORT_USERS:
             status = True
             break
     return status
