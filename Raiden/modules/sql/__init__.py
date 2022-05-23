e2bfa4e4da53f00117dd51dc322de226f362d296
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-05-23 16:02:26 +0530

    Update __init__.py

--- Raiden/modules/sql/__init__.py
+++ Raiden/modules/sql/__init__.py
@@ -1,21 +1,15 @@
+from Raiden import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from Raiden import DB_URL, LOGGER
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
     engine = create_engine(DB_URL, client_encoding="utf8")
-    LOGGER.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-try:
-    SESSION = start()
-except Exception as e:
-    LOGGER.exception(f"[PostgreSQL] Failed to connect due to {e}")
-    exit()
+SESSION = start()
